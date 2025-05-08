3ba36c72249d819ef7987667e322b335b37fca4e
Ankitaben Oza <ankitabenoza@Bhavishs-MacBook-Pro.local>
2025-05-08 10:56:20 -0400

    test

---
+++ src/main/java/com/demo/demo11/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.demo.demo11;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Ankita's Application!";
+    }
+}
